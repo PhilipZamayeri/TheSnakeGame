7ead7b689fd73a5dec85536ccf54db0a4752ced4
Philip Zamayeri <Philip.Zamayeri@gmail.com>
2020-12-08 11:04:05 +0100

    Added code to GameBoard and added object to GuiHandler methods

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Philip Zamayeri
  * Date: 2020-12-08
@@ -5,5 +8,25 @@
  * Project: TheSnakeGame
  * Copyright: MIT
  */
-public class GameBoard {
+public class GameBoard extends JPanel implements IBoard {
+    JButton newGame = new JButton("to Game over board");
+
+
+    public GameBoard(GuiHandler guiHandler) {
+        board(guiHandler);
+    }
+
+    @Override
+    public void board(GuiHandler guiHandler) {
+        setLayout(null);
+        setBackground(Color.BLACK);
+
+        newGame.setBounds(100, 200, 300, 50);
+        setVisible(true);
+
+
+
+        newGame.addActionListener(e -> guiHandler.changeToGameOverBoard());
+        add(newGame);
+    }
 }
