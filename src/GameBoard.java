f665fe2c1ece0e41c6311c9fa10b78e18bff8086
robinmartinsson <martinsson.r@gmail.com>
2020-12-09 17:50:29 +0100

    move methods. from WIP.

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -16,28 +16,29 @@ import java.util.Random;
  * Copyright: MIT
  */
 public class GameBoard extends JPanel implements IBoard {
+    GameLogic gl = new GameLogic();
     JPanel scorePanel = new JPanel();
     JPanel basePanel = new JPanel();
     JLabel scoreTxt = new JLabel("Score: ");
-    Layout position;
+//    Layout position;
 
     int rows = 15;
     int columns = 15;
     int unitSize = 25;
-    List<Layout> snake = new ArrayList<>();
+//    List<Layout> snake = new ArrayList<>();
     JLabel[][] labels = new JLabel[rows][columns];
 
-    Layout apple = new Layout(0, 0);
-    int lengthOfSnake = 3;
-    Boolean haveEaten = false;
-    char direction;
-    boolean moved = true;
-    int point = 1;
-    int score = 0;
+//    Layout apple = new Layout(0, 0);
+//    int lengthOfSnake = 3;
+//    Boolean haveEaten = false;
+//    char direction;
+//    boolean moved = true;
+//    int point = 1;
+//    int score = 0;
 
-    JLabel showScore = new JLabel("Score: " + score);
-    String bodyPart = '\u2584' + "";
-    String appleBit = '\u2058' + "";
+//    JLabel showScore = new JLabel("Score: " + score);
+//    String bodyPart = '\u2584' + "";
+//    String appleBit = '\u2058' + "";
 
     javax.swing.Timer timer; // Ambition att byta till Timer
     //Timer timer;
@@ -53,18 +54,18 @@ public class GameBoard extends JPanel implements IBoard {
         this.guiHandler = guiHandler;
         addLabels();
         board(guiHandler);
-        snake.clear();
-        createSnake();
+        gl.snake.clear();
+        gl.createSnake();
         markStartPosition();
-        shuffleApplePosition();
+        gl.shuffleApplePosition(labels, rows, columns);
         setKeyBindings();
 
         ActionListener time = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                move(direction,position);
-                moved = true;
-                updateSnake();
+                move(gl.direction,gl.position);
+                gl.moved = true;
+                gl.updateSnake(labels, rows, columns);
             }
         };
         timer = new javax.swing.Timer(100, time);
@@ -114,70 +115,70 @@ public class GameBoard extends JPanel implements IBoard {
         }
     }
 
-    public void createSnake() {
-        for (int i = 0; i < lengthOfSnake; i++) {
-            snake.add(new Layout(0, i));
-            if (i == lengthOfSnake - 1) position = new Layout(snake.get(i));
-        }
-    }
-
+//    public void createSnake() {
+//        for (int i = 0; i < lengthOfSnake; i++) {
+//            snake.add(new Layout(0, i));
+//            if (i == lengthOfSnake - 1) position = new Layout(snake.get(i));
+//        }
+//    }
+//
     public void markStartPosition() {
-        for (int i = 0; i < snake.size(); i++) {
-            var row = snake.get(i).row;
-            var column = snake.get(i).column;
-            labels[row][column].setText(bodyPart);
+        for (int i = 0; i < gl.snake.size(); i++) {
+            var row = gl.snake.get(i).row;
+            var column = gl.snake.get(i).column;
+            labels[row][column].setText(gl.bodyPart);
         }
-        direction = 'R';
+        gl.direction = 'R';
     }
-
-    public void shuffleApplePosition() {
-        labels[apple.row][apple.column].setForeground(null);
-        int row;
-        int col;
-        Random random = new Random();
-        while (true) {
-            row = random.nextInt(rows);
-            col = random.nextInt(columns);
-            if (position.row != row || position.column != col) break;
-        }
-        apple.newPos(row, col);
-        labels[apple.row][apple.column].setText(appleBit);
-        labels[apple.row][apple.column].setForeground(Color.red);
-    }
-
-    public void updateSnake() {
-        if (position.isEqualsTo(apple)) {
-            haveEaten = true;
-            addPoint();
-            shuffleApplePosition();
-        } else haveEaten = false;
-
-        snake.add(new Layout(position));
-
-        if (!haveEaten) {
-            labels[snake.get(0).row][snake.get(0).column].setText("");
-            labels[apple.row][apple.column].setText(appleBit);
-            snake.remove(0);
-        }
-
-        try { //läggs i en egen metod istället för try+catch
-            if (labels[position.row][position.column].getText().equals(bodyPart)) { //går in i sig själv
-                JOptionPane.showMessageDialog(null, "Game over! \nYour Score: " + score);
-                reset();
-                guiHandler.changeToGameOverBoard();
-            }
-            labels[position.row][position.column].setText(bodyPart);
-        } catch (IndexOutOfBoundsException e) { //går in i väggen
-            JOptionPane.showMessageDialog(null, "Game over! \nYour Score: " + score);
-            reset();
-            guiHandler.changeToGameOverBoard();
-        }
-    }
-
-    public void addPoint() {
-        score += point;
-        showScore.setText("Score: " + score);
-    }
+//
+//    public void shuffleApplePosition() {
+//        labels[apple.row][apple.column].setForeground(null);
+//        int row;
+//        int col;
+//        Random random = new Random();
+//        while (true) {
+//            row = random.nextInt(rows);
+//            col = random.nextInt(columns);
+//            if (position.row != row || position.column != col) break;
+//        }
+//        apple.newPos(row, col);
+//        labels[apple.row][apple.column].setText(appleBit);
+//        labels[apple.row][apple.column].setForeground(Color.red);
+//    }
+//
+//    public void updateSnake() {
+//        if (position.isEqualsTo(apple)) {
+//            haveEaten = true;
+//            addPoint();
+//            shuffleApplePosition();
+//        } else haveEaten = false;
+//
+//        snake.add(new Layout(position));
+//
+//        if (!haveEaten) {
+//            labels[snake.get(0).row][snake.get(0).column].setText("");
+//            labels[apple.row][apple.column].setText(appleBit);
+//            snake.remove(0);
+//        }
+//
+//        try { //läggs i en egen metod istället för try+catch
+//            if (labels[position.row][position.column].getText().equals(bodyPart)) { //går in i sig själv
+//                JOptionPane.showMessageDialog(null, "Game over! \nYour Score: " + score);
+//                reset();
+//                guiHandler.changeToGameOverBoard();
+//            }
+//            labels[position.row][position.column].setText(bodyPart);
+//        } catch (IndexOutOfBoundsException e) { //går in i väggen
+//            JOptionPane.showMessageDialog(null, "Game over! \nYour Score: " + score);
+//            reset();
+//            guiHandler.changeToGameOverBoard();
+//        }
+//    }
+//
+//    public void addPoint() {
+//        score += point;
+//        showScore.setText("Score: " + score);
+//    }
 
     public void reset() {
         for (int i = 0; i < rows; i++) {
@@ -185,13 +186,13 @@ public class GameBoard extends JPanel implements IBoard {
                 labels[i][j].setText("");
             }
         }
-        snake.clear();
-        createSnake();
+        gl.snake.clear();
+        gl.createSnake();
         markStartPosition();
-        shuffleApplePosition();
+        gl.shuffleApplePosition(labels,rows,columns);
         timer.stop();
-        score = -1;
-        addPoint();
+        gl.score = -1;
+        gl.addPoint();
     }
     private class KeyAction extends AbstractAction {
         public KeyAction(String actionCommand) {
@@ -202,23 +203,23 @@ public class GameBoard extends JPanel implements IBoard {
         public void actionPerformed(ActionEvent actionEvt) {
             switch (actionEvt.getActionCommand()) {
                 case VK_LEFT:
-                    if (direction != 'R') {
-                        direction = 'L';
+                    if (gl.direction != 'R') {
+                        gl.direction = 'L';
                     }
                     break;
                 case VK_RIGHT:
-                    if (direction != 'L') {
-                        direction = 'R';
+                    if (gl.direction != 'L') {
+                        gl.direction = 'R';
                     }
                     break;
                 case VK_UP:
-                    if (direction != 'D') {
-                        direction = 'U';
+                    if (gl.direction != 'D') {
+                        gl.direction = 'U';
                     }
                     break;
                 case VK_DOWN:
-                    if (direction != 'U') {
-                        direction = 'D';
+                    if (gl.direction != 'U') {
+                        gl.direction = 'D';
                     }
                     break;
             }
--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,5 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Philip Zamayeri
@@ -9,8 +12,90 @@ import java.util.List;
  * Copyright: MIT
  */
 public class GameLogic {
-    int rows;
-    int columns;
+//    int rows;
+//    int columns;
+    GameBoard gameBoard;
+    GuiHandler guiHandler;
+    Layout position;
+    List<Layout> snake = new ArrayList<>();
+    Layout apple = new Layout(0, 0);
+    int lengthOfSnake = 3;
+    Boolean haveEaten = false;
+    char direction;
+    boolean moved = true;
+    int point = 1;
+    int score = 0;
 
-    public GameLogic(){}
+
+    JLabel showScore = new JLabel("Score: " + score);
+    String bodyPart = '\u2584' + "";
+    String appleBit = '\u2058' + "";
+//    public GameLogic(){}
+
+
+    public void createSnake() {
+        for (int i = 0; i < lengthOfSnake; i++) {
+            snake.add(new Layout(0, i));
+            if (i == lengthOfSnake - 1) position = new Layout(snake.get(i));
+        }
+    }
+
+//    public void markStartPosition() {
+//        for (int i = 0; i < snake.size(); i++) {
+//            var row = snake.get(i).row;
+//            var column = snake.get(i).column;
+//            labels[row][column].setText(bodyPart);
+//        }
+//        direction = 'R';
+//    }
+
+    public void shuffleApplePosition(JLabel[][] labels,int rows, int columns) {
+        labels[apple.row][apple.column].setForeground(null);
+        int row;
+        int col;
+        Random random = new Random();
+        while (true) {
+            row = random.nextInt(rows);
+            col = random.nextInt(columns);
+            if (position.row != row || position.column != col) break;
+        }
+        apple.newPos(row, col);
+        labels[apple.row][apple.column].setText(appleBit);
+        labels[apple.row][apple.column].setForeground(Color.red);
+    }
+
+    public void updateSnake(JLabel[][] labels, int rows, int columns) {
+        if (position.isEqualsTo(apple)) {
+            haveEaten = true;
+            addPoint();
+            shuffleApplePosition(labels, rows, columns);
+        } else haveEaten = false;
+
+        snake.add(new Layout(position));
+
+        if (!haveEaten) {
+            labels[snake.get(0).row][snake.get(0).column].setText("");
+            labels[apple.row][apple.column].setText(appleBit);
+            snake.remove(0);
+        }
+
+        try { //läggs i en egen metod istället för try+catch
+            if (labels[position.row][position.column].getText().equals(bodyPart)) { //går in i sig själv
+                JOptionPane.showMessageDialog(null, "Game over! \nYour Score: " + score);
+                gameBoard.reset();
+                guiHandler.changeToGameOverBoard();
+            }
+            labels[position.row][position.column].setText(bodyPart);
+        } catch (IndexOutOfBoundsException e) { //går in i väggen
+            JOptionPane.showMessageDialog(null, "Game over! \nYour Score: " + score);
+            gameBoard.reset();
+            guiHandler.changeToGameOverBoard();
+        }
+    }
+
+    public void addPoint() {
+        score += point;
+        showScore.setText("Score: " + score);
+    }
+
 }
