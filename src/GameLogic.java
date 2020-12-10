8cc276b472dba8bea710695e6616bbd4b7704642
SanaJava <sana.eneroth@gmail.com>
2020-12-10 13:42:29 +0100

    Fixed reset method problem, moved methods from GB to GL.

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,19 +1,32 @@
 import javax.swing.*;
-import javax.swing.border.EtchedBorder;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-
-/**
- * Created by Philip Zamayeri
- * Date: 2020-12-08
- * Time: 14:17
- * Project: TheSnakeGame
- * Copyright: MIT
- */
 public class GameLogic {
+
+    GameBoard gameBoard;
+    GuiHandler guiHandler;
+    Layout position;
+    List<Layout> snake = new ArrayList<>();
+    Layout apple = new Layout(0, 0);
+    int lengthOfSnake = 3;
+    Boolean haveEaten = false;
+    char direction;
+    boolean moved = true;
+    int point = 1;
+    int score = 0;
+
+    JLabel showScore = new JLabel("Score: " + score);
+    String bodyPart = '\u2584' + "";
+    String appleBit = '\u2058' + "";
+
+    public GameLogic(GameBoard gameBoard, GuiHandler guiHandler) {
+        this.gameBoard = gameBoard;
+        this.guiHandler = guiHandler;
+    }
+
 
     public void move(char direction, Layout position) {
         if (direction == 'U') position.row--;
@@ -22,20 +35,16 @@ public class GameLogic {
         if (direction == 'R') position.column++;
     }
 
-    public void addLabels(JPanel basePanel, JLabel[][] labels, int rows, int columns, int unitSize) {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                labels[i][j] = new JLabel("", SwingConstants.CENTER);
-                //labels[i][j].setBorder(new EtchedBorder()); // om detta används sätt fontsize till 20
-                labels[i][j].setPreferredSize(new Dimension(unitSize, unitSize));
-                labels[i][j].setFont(new Font("Andale Mono", Font.BOLD, 30)); // sätt till 20 om border nyttjas
-                basePanel.add(labels[i][j]);
-                //labels[i][j].setForeground(Color.GREEN);
-            }
+    public void markStartPosition(JLabel[][] labels) {
+        for (int i = 0; i < snake.size(); i++) {
+            var row = snake.get(i).row;
+            var column = snake.get(i).column;
+            labels[row][column].setText(bodyPart);
         }
+        direction = 'R';
     }
 
-    public void shuffleApplePosition(JLabel[][] labels,Layout apple, Layout position, String appleBit, int rows, int columns) {
+    public void shuffleApplePosition(JLabel[][] labels, int rows, int columns) {
         labels[apple.row][apple.column].setForeground(null);
         int row;
         int col;
@@ -50,4 +59,38 @@ public class GameLogic {
         labels[apple.row][apple.column].setForeground(Color.red);
     }
 
+    public void updateSnake(JLabel[][] labels, int rows, int columns) {
+        if (position.isEqualsTo(apple)) {
+            haveEaten = true;
+            addPoint();
+            shuffleApplePosition(labels, rows, columns);
+        } else haveEaten = false;
+
+        snake.add(new Layout(position));
+
+        if (!haveEaten) {
+            labels[snake.get(0).row][snake.get(0).column].setText("");
+            labels[apple.row][apple.column].setText(appleBit);
+            snake.remove(0);
+        }
+
+        try {
+            if (labels[position.row][position.column].getText().equals(bodyPart)) {
+                System.out.println("Game over! \nYour Score: " + score);
+                gameBoard.reset();
+                //gameBoard.timer.stop();
+                guiHandler.changeToGameOverBoard();
+            }
+            labels[position.row][position.column].setText(bodyPart);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Game over! \nYour Score: " + score);
+            gameBoard.reset();
+            guiHandler.changeToGameOverBoard();
+        }
+    }
+
+    public void addPoint() {
+        score += point;
+        showScore.setText("Score: " + score);
+    }
 }
